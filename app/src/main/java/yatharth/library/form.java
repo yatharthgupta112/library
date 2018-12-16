f5620bbc0d4f8cf8eb19694700116703a665e1fd
yatharthgupta112 <yatharthgupta112@gmail.com>
2018-12-17 04:25:39 +0530

    form fourth

--- app/src/main/java/yatharth/library/form.java
+++ app/src/main/java/yatharth/library/form.java
@@ -1,7 +1,10 @@
 package yatharth.library;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class form extends AppCompatActivity {
 
@@ -9,7 +12,13 @@ public class form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button submit = findViewById(R.id.submit_button);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), form2.class));
+            }
+        });
 
     }
 }
