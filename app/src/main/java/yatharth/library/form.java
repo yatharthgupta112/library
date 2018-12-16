12eb573da7f628faf3201efc07e1f74b9e0bf78b
yatharthgupta112 <yatharthgupta112@gmail.com>
2018-12-17 03:42:36 +0530

    form second

--- app/src/main/java/yatharth/library/form.java
+++ app/src/main/java/yatharth/library/form.java
@@ -3,7 +3,7 @@ package yatharth.library;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class form extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
